aeb31b210465396f636344edef7274fc3814e005
milenakosh <147971990+milenakosh@users.noreply.github.com>
2024-10-18 22:18:57 +0300

    hui

---
+++ дз или практика/script.js
@@ -0,0 +1,41 @@
+let sfetafor = "red";
+let tim = "yellow";
+let gfgf = "green";
+if (sfetafor === "stop") {
+    console.log("надо стоять");
+
+} else if (tim === "Get ready") {
+    console.log("надо приготовится");
+}
+else {
+    (gfgf === "green")
+    console.log("можно идти");
+}
+
+
+let n = 23;
+let m = 45;
+let nm = (+n + +m);
+console.log(`${n + m}`);
+n = parseFloat(n);
+m = parseFloat(m);
+
+
+
+let num = 10;
+let res;
+res = num == 10 ? 0 : 5;
+console.log(`${num * res}`);
+
+
+let num3 = 0;
+let res1;
+res1 = num3 == 10 ? 10 : 20;
+console.log(res1);
+
+let num1 = 2;
+let num2 = 3;
+if (num1 == 2 + num2 == 3) {
+    console.log('+++');
+}
+
